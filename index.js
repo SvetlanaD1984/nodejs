c8b98fb81ca4d589f8e13c28a5f9b3933d1e7211
svetlana <developer-svetlana@yandex.ru>
2022-03-26 09:27:34 +0500

    dz3

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-require('colors')
+const fs = require("fs");
+const readline = require("readline");
 
-const Colors = {GREEN : 0, YELLOW: 1, RED : 2}
+const readStream = fs.createReadStream("./access.log", "utf8");
+const writeStream1 = fs.createWriteStream("./89.123.1.41_requests.log");
+const writeStream2 = fs.createWriteStream("./34.48.240.111_requests.log");
 
-let currentColor = Colors.GREEN;
-const leftRest = +process.argv[2];
-const rightRest = +process.argv[3];
-let noPrimeNum = true;
+let numStr = 0
 
-if(isNaN(leftRest) || isNaN(rightRest)){
-    console.log('Incorrect start parameters'.red);
-    return;
-}
+const rl = readline.createInterface({
+  input: readStream,
+  terminal: true,
+});
 
-const isPrimeNum = (num) => {
-    if (num <= 1)
-        return false;
-    for(let i = 2; i < num; i++)
-        if(num % i === 0) return false;
-    return true;
-}
-const changeColor = () => {
-    currentColor++;
-    if (currentColor > Colors.RED)
-        currentColor = Colors.GREEN;
-}
+rl.on("line", (line) => {
+  if (line.includes("89.123.1.41")) {
+    writeStream1.write(line + "\n");
+  }
+
+  if (line.includes("34.48.240.111")) {
+    writeStream2.write(line + "\n");
+  }
+  console.log(++numStr)
+});
