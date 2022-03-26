8992549d163605f5dfca0763fdea93da3cc67885
svetlana <developer-svetlana@yandex.ru>
2022-03-26 09:23:50 +0500

    dz2

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
-require('colors')
+require("moment-precise-range-plugin");
+const moment = require("moment");
+const EventEmitter = require("events");
+const [dateStringInFuture] = process.argv.slice(2);
+const DATE_FORMAT_PATTERN = "YYYY-MM-DD HH:mm:ss";
 
-const Colors = {GREEN : 0, YELLOW: 1, RED : 2}
+/**
+ * @param dateString
+ * @returns {Date}
+ */
+const getDateFromDateString = (dateString) => {
 
-let currentColor = Colors.GREEN;
-const leftRest = +process.argv[2];
-const rightRest = +process.argv[3];
-let noPrimeNum = true;
+  const [hour, day, month, year] = dateString.split("-");
 
-if(isNaN(leftRest) || isNaN(rightRest)){
-    console.log('Incorrect start parameters'.red);
-    return;
-}
+  return new Date(Date.UTC(year, month - 1, day, hour));
+};
 
-const isPrimeNum = (num) => {
-    if (num <= 1)
-        return false;
-    for(let i = 2; i < num; i++)
-        if(num % i === 0) return false;
-    return true;
-}
-const changeColor = () => {
-    currentColor++;
-    if (currentColor > Colors.RED)
-        currentColor = Colors.GREEN;
-}
+/**
+ * @param {Date} dateInFuture
+ */
+const showRemainingTime = (dateInFuture) => {
+  const dateNow = new Date();
+
+  if (dateNow >= dateInFuture) {
+     emitter.emit("timerEnd");
+  } else {
+    const currentDateFormatted = moment(dateNow, DATE_FORMAT_PATTERN);
+    const futureDateFormatted = moment(dateInFuture, DATE_FORMAT_PATTERN);
+    const diff = moment.preciseDiff(currentDateFormatted, futureDateFormatted);
+
+    console.clear();
+    console.log(diff);
+  }
+};
+
+/**
+ * @param {Number} timerId
+ */
+const showTimerDone = (timerId) => {
+  clearInterval(timerId);
+  console.log("Таймер истек");
+};
+
+const emitter = new EventEmitter();
+const dateInFuture = getDateFromDateString(dateStringInFuture);
+const timerId = setInterval(() => {
+  emitter.emit("timerTick", dateInFuture);
+}, 1000);
+
+emitter.on("timerTick", showRemainingTime);
+emitter.on("timerEnd", () => {
+  showTimerDone(timerId);
+});
