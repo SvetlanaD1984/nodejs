f2ebd00275b67ba52981f4d4c009317732a7c4c2
svetlana <developer-svetlana@yandex.ru>
2022-05-18 11:09:28 +0500

    dz6

--- index.js
+++ index.js
@@ -1,47 +1,79 @@
-const colors = require("colors");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const io = require("socket.io");
 
-const Colors = { GREEN: 0, YELLOW: 1, RED: 2 };
+const app = http.createServer((request, response) => {
+  if (request.method === "GET") {
+    const filePath = path.join(__dirname, "index.html");
+    readStream = fs.createReadStream(filePath);
+    readStream.pipe(response);
+  } else if (request.method === "POST") {
+    let data = "";
+    request.on("data", (chunk) => {
+      data += chunk;
+    });
 
-let currentColor = Colors.GREEN;
-const leftRest = +process.argv[2];
-const rightRest = +process.argv[3];
-let noPrimeNum = true;
-
-if (isNaN(leftRest) || isNaN(rightRest)) {
-  console.log("Incorrect start parameters".red);
-  return;
-}
-
-const isPrimeNum = (num) => {
-  if (num <= 1) return false;
-  for (let i = 2; i < num; i++) if (num % i === 0) return false;
-  return true;
-};
-const changeColor = () => {
-  currentColor++;
-  if (currentColor > Colors.RED) currentColor = Colors.GREEN;
-};
-
-const colorPrint = (num) => {
-  if (noPrimeNum) noPrimeNum = false;
-  switch (currentColor) {
-    case Colors.RED:
-      console.log(`${num}`.red);
-      break;
-    case Colors.GREEN:
-      console.log(`${num}`.green);
-      break;
-    case Colors.YELLOW:
-      console.log(`${num}`.yellow);
-      break;
+    request.on("end", () => {
+      const parsedData = JSON.parse(data);
+      console.log(parsedData);
+      response.writeHead(200, { "Content-Type": "json" });
+      response.end(data);
+    });
+  } else {
+    response.statusCode = 405;
+    response.end();
   }
-  changeColor();
-};
+});
 
-for (let i = leftRest; i <= rightRest; i++) {
-  if (isPrimeNum(i)) colorPrint(i);
-}
-if (noPrimeNum)
-  console.log(
-    `There are no primes in this range[${leftRest},${rightRest}]`.red
-  );
+const socket = io(app);
+
+let users = [];
+
+socket.on("connection", function (socket) {
+  console.log("New connection");
+
+  socket.on("NEW_LOGIN_EVENT", (username) => {
+    socket.emit("SERVER_MSG", { msg: username + " " + "зашёл в чат" });
+  });
+
+  socket.on("NEW_LOGIN_EVENT", (username) => {
+    socket.broadcast.emit("NEW_CONN_EVENT", {
+      msg: username + " " + "зашёл в чат",
+    });
+  });
+
+  socket.on("disconnect", function (username) {
+    console.log("The client disconnected");
+
+    socket.broadcast.emit("NEW_DISCONN_EVENT", {
+      msg: socket.username + " " + "вышел из чата",
+    });
+  });
+
+  socket.on("ADD_USERS", (username) => {
+    function addUser(username) {
+      users.push({
+        username: username,
+      });
+    }
+
+    addUser(` ${username} `);
+
+    users.map((username, i) => (username.id = i + 1));
+
+    console.log(users);
+  });
+
+  socket.on("TYPING", (username) => {
+    socket.broadcast.emit("SERVER_MSG", {
+      msg: username + " " + "печатает сообщение",
+    });
+  });
+
+  socket.on("CLIENT_MSG", (data) => {
+    socket.emit("SERVER_MSG", { msg: data.msg.split("").reverse().join("") });
+  });
+});
+
+app.listen(3000, "localhost");
